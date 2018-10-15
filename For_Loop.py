d58b3e3b4f34e752f30701555f58b62fe775573e
Andre Williams <awjrs22@gmail.com>
2018-10-15 17:57:56 -0500

    For_Loop.py

---
+++ For_Loop.py
@@ -0,0 +1,15 @@
+# A for loop is used to iterate through a list
+
+exampleList = [1, 5, 6, 3, 5, 23, 45, 63, 78, 98, 9, 4, 2, 14, 26, 5]
+# this creates a variable called eachNumber that python recognizes inside the list
+for eachNumber in exampleList: 
+	print(eachNumber)
+	print('continue program')
+
+# Range is a built in python function so you don't have to import it
+for x in range(1, 11):
+	print(x)
+
+
+
+
