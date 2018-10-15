3800492b3ec3d4b59c192748fd7574fa4aa782dd
Andre Williams <awjrs22@gmail.com>
2018-10-15 18:17:49 -0500

    Statistics_Mean_STD.py

---
+++ Statistics_Mean_STD.py
@@ -0,0 +1,12 @@
+# These are basic math functions you can do with python
+# Mean, STD, Mode, Median, Variance
+
+import statistics
+
+examples_list = [4, 5, 6, 2, 25, 4, 24, 8, 9, 34, 32, 25, 46, 74, 75, 34, 73, 2, 4, 22]
+ # Now you can use the statistics model to do stuff with this data
+
+ x = statistics.mean(examples_list)
+
+print(x)
+
