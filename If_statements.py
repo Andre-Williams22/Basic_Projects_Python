b32fee81ceaf19ad56db95ebc2b1ca4ba9ac06be
Andre Williams <awjrs22@gmail.com>
2018-10-15 18:04:35 -0500

    If_statements.py

---
+++ If_statements.py
@@ -0,0 +1,14 @@
+# Is used to determine "if something is supposed to be something" or else stay the same
+
+x = 5 
+y = 8 
+z = 5 
+a = 3
+
+
+if z < y > x > a:
+	print('y is greater than z and greater than x')
+# a double equals sign means if z is equal to x
+# != is does not equal
+if z == x:
+	print('z is less than or equal to x')
