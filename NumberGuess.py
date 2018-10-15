c65bf30718d1ad66259e904e24e2f1d708abe782
Andre Williams <awjrs22@gmail.com>
2018-10-15 17:59:17 -0500

    NumberGuess.py

---
+++ NumberGuess.py
@@ -0,0 +1,4 @@
+"""This program randomly rolls a pair of dice and adds the values of the roll.
+It then asks the user to guess a number, compares the user's guess to the total value and finally decides whether the winner is the user or the program and informs the user who the winner is. The Program shows competence in identifying and calling functions"""
+
+from random import randint
