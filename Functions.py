58be5d7f1935870a1cda22fa541f22bf61cb6abe
Andre Williams <awjrs22@gmail.com>
2018-10-15 18:03:16 -0500

    Functions.py

---
+++ Functions.py
@@ -0,0 +1,7 @@
+#parenthesis are the parameters
+def example():
+	print('basic function')
+	z = 3+8 
+	print(z)
+	# To call the function just type out the name of the function outside of the function
+example()
