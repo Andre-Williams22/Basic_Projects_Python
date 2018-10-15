d2d96701a7c6f982772d3c80ff6402441fe3fdae
Andre Williams <awjrs22@gmail.com>
2018-10-15 18:23:30 -0500

    Else_if.py

---
+++ Else_if.py
@@ -0,0 +1,13 @@
+x = 5 
+y = 20 
+z = 22
+
+
+if x > y:
+	print('x is greater than y')
+elif x == z:
+	print('x is less than z')
+elif 5 == 2:
+	print('5 is greater than 2')
+else: 
+	print('if and elif(s) never ran')
