2fc9dc32b9c10f0740f861f0ab0da60efb187194
Andre Williams <awjrs22@gmail.com>
2018-10-15 18:20:22 -0500

    Function_Parameters.py

---
+++ Function_Parameters.py
@@ -0,0 +1,7 @@
+# Pick any name you want in the parameters. They're going to act like variable names
+def simple_addition(num1, num2):
+	answer = num1+ num2
+	print('num1 is', num1)
+	print(answer)
+
+simple_addition(5,3)
