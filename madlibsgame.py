6fc0443a4ad172900c0695a029644f5fa7b85702
Andre Williams <awjrs22@gmail.com>
2019-02-02 18:20:41 -0600

    madlibsgame.py

---
+++ madlibsgame.py
@@ -0,0 +1,13 @@
+# This the madlibs game which if you don't know already is frequently played at parties. 
+# Mad Libs is a phrasal template word game where one player prompts others for a list of words to substitute for blanks in a story, before reading the – often comical or nonsensical – story aloud. 
+color = input(" Enter a color: ")
+plural_noun = input(" Enter a plural noun: ")
+celebrity = input(" Enter a celebrity: ")
+
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+print("How about you ?")
+
