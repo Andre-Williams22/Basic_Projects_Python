fdfc46f870e3570e483545491ef0b577b0acd4dd
Andre <awjrs22@gmail.com>
2019-08-27 13:46:23 -0700

    for_loop.py

--- For_Loop.py
+++ For_Loop.py
@@ -2,14 +2,10 @@
 
 exampleList = [1, 5, 6, 3, 5, 23, 45, 63, 78, 98, 9, 4, 2, 14, 26, 5]
 # this creates a variable called eachNumber that python recognizes inside the list
-for eachNumber in exampleList: 
+for eachNumber in exampleList:
 	print(eachNumber)
 	print('continue program')
 
 # Range is a built in python function so you don't have to import it
 for x in range(1, 11):
 	print(x)
-
-
-
-
